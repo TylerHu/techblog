d3528517898ad69c6255bf4daf24c42008883e17
tyler <zhnhu@foxmail.com>
2017-02-12 17:28:30 +0800

    typo

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-            'sqlite:///' + os.path.         join(basedir, 'data-dev.sqlite')
+            'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 config = {
         'development': DevelopmentConfig,
