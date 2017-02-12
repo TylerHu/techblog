37208dd6086a26e2c4d031f43ed2da367b4f5c42
tyler <zhnhu@foxmail.com>
2017-02-12 17:25:25 +0800

    fixed untracked files refs:http://stackoverflow.com/questions/11451535/gitignore-is-not-working

--- config.py
+++ config.py
@@ -13,7 +13,8 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.         join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+            'sqlite:///' + os.path.         join(basedir, 'data-dev.sqlite')
 
 config = {
         'development': DevelopmentConfig,
